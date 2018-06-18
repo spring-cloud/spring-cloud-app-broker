805e964c28b98a966e34c3a159fd6287e82d7f24
Ollie Hughes <ohughes@pivotal.io>
2018-06-18 17:58:27 +0100

    Fix CI error by removing broken references in autoconfiguration

--- spring-cloud-app-broker-autoconfigure/src/main/java/org/springframework/cloud/appbroker/config/WorkflowConfiguration.java
+++ spring-cloud-app-broker-autoconfigure/src/main/java/org/springframework/cloud/appbroker/config/WorkflowConfiguration.java
@@ -3,28 +3,10 @@ package org.springframework.cloud.appbroker.config;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.appbroker.model.ServiceInstance;
-import org.springframework.cloud.appbroker.pipeline.output.DeployedServices;
-import org.springframework.cloud.appbroker.pipeline.output.GeneratedCredentials;
-import org.springframework.cloud.appbroker.pipeline.output.PersistResponse;
-import org.springframework.cloud.appbroker.pipeline.output.TransformedParameters;
-import org.springframework.cloud.appbroker.pipeline.step.InMemoryPersistenceService;
-import org.springframework.cloud.appbroker.pipeline.step.PersistenceService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WorkflowConfiguration {
 
-	@ConditionalOnMissingBean
-	@Bean
-	public PersistenceService<TransformedParameters<?>, DeployedServices<?>, DeployedApp<?>, GeneratedCredentials<?>, PersistResponse<?>>
-	createServicePersistenceStep(ConcurrentHashMap<String, ServiceInstance> inMemoryStore) {
-		return new InMemoryPersistenceService(inMemoryStore);
-	}
-
-	@Bean
-	ConcurrentHashMap<String, ServiceInstance> inMemoryStore() {
-		return new ConcurrentHashMap<>();
-	}
 }

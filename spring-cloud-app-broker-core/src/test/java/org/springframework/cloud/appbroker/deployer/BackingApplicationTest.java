66190335d0a9ab562030bada5409117470f5d306
Alexey Nesterov <alexey.nesterov@live.com>
2018-10-03 10:01:24 +0100

    Hide potentially sensitive logs output when serializing backing app
    Closes
    https://github.com/spring-cloud-incubator/spring-cloud-app-broker/issues/107

---
+++ spring-cloud-app-broker-core/src/test/java/org/springframework/cloud/appbroker/deployer/BackingApplicationTest.java
@@ -0,0 +1,29 @@
+package org.springframework.cloud.appbroker.deployer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BackingApplicationTest {
+
+	@Test
+	void stringRepresentationShouldNotExposeSensitiveInformationFromTheEnvironment() {
+		BackingApplication backingApp = BackingApplication
+			.builder()
+			.name("Test")
+			.environment("privateKey", "secret-private-key")
+			.environment("databasePassword", "password")
+			.build();
+
+		String backingAppAsString = backingApp.toString();
+
+		assertThat(backingAppAsString).doesNotContain("secret-private-key");
+		assertThat(backingAppAsString).doesNotContain("password");
+
+		assertThat(backingAppAsString).contains("privateKey=<value hidden>");
+		assertThat(backingAppAsString).contains("databasePassword=<value hidden>");
+
+		assertThat(backingApp.getEnvironment().get("privateKey")).isEqualTo("secret-private-key");
+		assertThat(backingApp.getEnvironment().get("databasePassword")).isEqualTo("password");
+	}
+}

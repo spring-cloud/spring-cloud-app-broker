7f5ce4941daaca4d7d406cea901f2261cb98b262
Scott Frederick <scottyfred@gmail.com>
2018-12-11 16:55:23 -0600

    Create a spring-cloud-starter-app-broker-cloudfoundry and move all CF-specific dependencies.

--- spring-cloud-app-broker-sample/src/test/java/org.springframework.cloud.appbroker/sample/WiremockComponentTest.java
+++ spring-cloud-app-broker-sample/src/test/java/org.springframework.cloud.appbroker/sample/WiremockComponentTest.java
@@ -34,7 +34,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
 	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-	classes = {AppBrokerSampleApplication.class,
+	classes = {AppBrokerApplication.class,
 		WiremockServerFixture.class,
 		OpenServiceBrokerApiFixture.class,
 		CloudControllerStubFixture.class,

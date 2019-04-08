8517767fec929a8d3d7e9975d48f7c850f58dc92
Raul Avila <ravila@pivotal.io>
2019-04-08 16:39:44 +0100

    Find organization to access service instance
    - `CloudFoundryOperationUtils` was accessing the default org
      to get operations, which in some cases wasn't correct, e.g.
      when getting a service instance not created in the default org.
      To solve this problem, we're now exposing a new method `getOperationsForOrgAndSpace`,
      which gets the operations in the organization and space specified.

--- spring-cloud-app-broker-deployer-cloudfoundry/src/main/java/org/springframework/cloud/appbroker/deployer/cloudfoundry/CloudFoundryOperationsUtils.java
+++ spring-cloud-app-broker-deployer-cloudfoundry/src/main/java/org/springframework/cloud/appbroker/deployer/cloudfoundry/CloudFoundryOperationsUtils.java
@@ -33,7 +33,7 @@ public class CloudFoundryOperationsUtils {
 		this.operations = operations;
 	}
 
-	public Mono<CloudFoundryOperations> getOperations(Map<String, String> properties) {
+	Mono<CloudFoundryOperations> getOperations(Map<String, String> properties) {
 		return Mono.defer(() -> {
 			if (!CollectionUtils.isEmpty(properties) && properties.containsKey(
 				DeploymentProperties.TARGET_PROPERTY_KEY)) {
@@ -43,12 +43,22 @@ public class CloudFoundryOperationsUtils {
 		});
 	}
 
-	public Mono<CloudFoundryOperations> getOperationsForSpace(String space) {
+	Mono<CloudFoundryOperations> getOperationsForSpace(String space) {
 		return Mono.just(this.operations)
-			.cast(DefaultCloudFoundryOperations.class)
-			.map(cfOperations -> DefaultCloudFoundryOperations.builder()
-				.from(cfOperations)
-				.space(space)
-				.build());
+				   .cast(DefaultCloudFoundryOperations.class)
+				   .map(cfOperations -> DefaultCloudFoundryOperations.builder()
+																	 .from(cfOperations)
+																	 .space(space)
+																	 .build());
+	}
+
+	Mono<CloudFoundryOperations> getOperationsForOrgAndSpace(String organization, String space) {
+		return Mono.just(this.operations)
+				   .cast(DefaultCloudFoundryOperations.class)
+				   .map(cfOperations -> DefaultCloudFoundryOperations.builder()
+																	 .from(cfOperations)
+																	 .organization(organization)
+																	 .space(space)
+																	 .build());
 	}
 }

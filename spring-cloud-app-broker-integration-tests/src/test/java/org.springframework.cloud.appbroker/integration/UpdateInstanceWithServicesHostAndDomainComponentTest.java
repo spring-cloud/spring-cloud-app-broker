ec1cd4ee6abac571db08f4a411da237d0e1a1433
Alberto Rios <arios@pivotal.io>
2019-09-13 12:42:10 +0200

    Adding component test

---
+++ spring-cloud-app-broker-integration-tests/src/test/java/org.springframework.cloud.appbroker/integration/UpdateInstanceWithServicesHostAndDomainComponentTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2002-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.appbroker.integration;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.appbroker.integration.fixtures.CloudControllerStubFixture;
+import org.springframework.cloud.appbroker.integration.fixtures.OpenServiceBrokerApiFixture;
+import org.springframework.cloud.servicebroker.model.instance.OperationState;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.cloud.appbroker.integration.UpdateInstanceWithServicesHostAndDomainComponentTest.APP_NAME;
+
+@TestPropertySource(properties = {
+		"spring.cloud.appbroker.services[0].service-name=example",
+		"spring.cloud.appbroker.services[0].plan-name=standard",
+		"spring.cloud.appbroker.services[0].apps[0].path=classpath:demo.jar",
+		"spring.cloud.appbroker.services[0].apps[0].name=" + APP_NAME,
+		"spring.cloud.appbroker.services[0].apps[0].parameters-transformers[0].name=PropertyMapping",
+		"spring.cloud.appbroker.services[0].apps[0].parameters-transformers[0].args.include=host,domain"
+})
+class UpdateInstanceWithServicesHostAndDomainComponentTest extends WiremockComponentTest {
+
+	static final String APP_NAME = "app-update-hostname-param";
+
+	@Autowired
+	private OpenServiceBrokerApiFixture brokerFixture;
+
+	@Autowired
+	private CloudControllerStubFixture cloudControllerFixture;
+
+	@Test
+	void updateAppWithHostAndDomain() {
+		cloudControllerFixture.stubAppExists(APP_NAME);
+		cloudControllerFixture.stubUpdateAppWithHostAndDomain(APP_NAME);
+
+		HashMap<String, Object> expectedUpdateParams = new HashMap<>();
+		expectedUpdateParams.put("host", "myhost");
+		expectedUpdateParams.put("domain", "my.domain.com");
+
+		// when a service instance is updated with parameters
+		given(brokerFixture.serviceInstanceRequest(expectedUpdateParams))
+				.when()
+				.patch(brokerFixture.createServiceInstanceUrl(), "instance-id")
+				.then()
+				.statusCode(HttpStatus.ACCEPTED.value());
+
+		// when the "last_operation" API is polled
+		given(brokerFixture.serviceInstanceRequest())
+				.when()
+				.get(brokerFixture.getLastInstanceOperationUrl(), "instance-id")
+				.then()
+				.statusCode(HttpStatus.OK.value())
+				.body("state", is(equalTo(OperationState.IN_PROGRESS.toString())));
+
+		String state = brokerFixture.waitForAsyncOperationComplete("instance-id");
+		assertThat(state).isEqualTo(OperationState.SUCCEEDED.toString());
+	}
+}

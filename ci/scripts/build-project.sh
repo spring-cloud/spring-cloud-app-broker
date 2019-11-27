14d5d26bbe2bc85cc76229f2405d619e5567a5c9
Alberto Rios <arios@pivotal.io>
2019-11-27 18:03:13 +0100

    Increasing gradle memory

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -6,5 +6,5 @@ source "$(dirname "$0")/common.sh"
 repository=$(pwd)/distribution-repository
 
 pushd git-repo >/dev/null
-./gradlew --no-daemon clean build install -Dmaven.repo.local="${repository}"
+./gradlew --no-daemon clean build install -Dmaven.repo.local="${repository}" -Dorg.gradle.jvmargs="-Xmx512m -Xmx2048m"
 popd >/dev/null

fc1d26f66e00c49db63afbb7bbb1976ef25450cd
Gareth Clay <gclay@pivotal.io>
2020-06-09 17:16:23 +0100

    Fix CI log interleaving
    * Stop logs from parallel tests interleaving with each other
    * Cut down on noise and reduce page load times by only logging std
    streams for tests that fail
    * @Validated AT cloud foundry configuration
    * Remove unnecessary Gradle tasks from the AT job since they've already
    been performed as part of `build`
    * Stop tests and javadoc Gradle tasks being run against docs subproject
    and causing failures
    
    #380

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+readonly ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE:"true"}"
+
 # shellcheck source=scripts/common.sh
 source "$(dirname "$0")/common.sh"
 repository=$(pwd)/distribution-repository
 
 pushd git-repo >/dev/null
-./gradlew --no-daemon clean build install -Dmaven.repo.local="${repository}" -Dorg.gradle.jvmargs="-Xmx512m -Xmx2048m"
+./gradlew --no-daemon clean build install \
+	-PonlyShowStandardStreamsOnTestFailure="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE}" \
+	-Dmaven.repo.local="${repository}" \
+	-Dorg.gradle.jvmargs="-Xmx512m -Xmx2048m"
 popd >/dev/null

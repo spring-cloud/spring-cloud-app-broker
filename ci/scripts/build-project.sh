81ec2f15a3edd43cff77bb3dc402216339da76d7
Gareth Clay <gclay@pivotal.io>
2020-07-02 12:02:31 +0100

    Update publication config
    * Allows publication to a custom maven repo, as we do in CI tasks
    * Adds `SKIP_TESTS` (defaults to `false`) param for quicker testing
    of CI build task with `fly execute`

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -3,13 +3,19 @@
 set -euo pipefail
 
 readonly ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE:-true}"
+readonly SKIP_TESTS="${SKIP_TESTS:-false}"
 
 # shellcheck source=common.sh
 source "$(dirname "$0")/common.sh"
 repository=$(pwd)/distribution-repository
+if [ "$SKIP_TESTS" == "true" ]; then
+	build_task=assemble
+else
+	build_task=build
+fi
 
 pushd git-repo >/dev/null
-./gradlew --parallel clean build install \
+./gradlew --parallel clean "$build_task" publish \
 	-PonlyShowStandardStreamsOnTestFailure="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE}" \
-	-Dmaven.repo.local="${repository}"
+	-PpublicationRepository="${repository}"
 popd >/dev/null

bd84a35a8fedfecd65ae4f596c7e1e1c17de3ead
Gareth Clay <gclay@pivotal.io>
2021-05-07 13:48:50 +0100

    Use Toolsmith environment for running ATs

--- ci/scripts/acceptance-tests.sh
+++ ci/scripts/acceptance-tests.sh
@@ -2,16 +2,35 @@
 
 set -euo pipefail
 
-readonly API_HOST="${API_HOST:?must be set}"
-readonly API_PORT="${API_PORT:?must be set}"
-readonly USERNAME="${USERNAME:?must be set}"
-readonly PASSWORD="${PASSWORD:?must be set}"
-readonly CLIENT_ID="${CLIENT_ID:?must be set}"
-readonly CLIENT_SECRET="${CLIENT_SECRET:?must be set}"
+readonly TOOLSMITH_ENV_INPUT="${TOOLSMITH_ENV_INPUT:?must be set}"
 readonly DEFAULT_ORG="${DEFAULT_ORG:?must be set}"
 readonly DEFAULT_SPACE="${DEFAULT_SPACE:?must be set}"
 readonly SKIP_SSL_VALIDATION="${SKIP_SSL_VALIDATION:?must be set}"
 readonly ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE:-true}"
+
+declare API_HOST
+readonly API_PORT=443
+readonly USERNAME="admin"
+declare PASSWORD
+readonly CLIENT_ID="admin"
+declare CLIENT_SECRET
+
+discover_environment() {
+  local env_name
+  env_name=$(cat "$TOOLSMITH_ENV_INPUT/name")
+
+  eval "$(bbl print-env --metadata-file "$TOOLSMITH_ENV_INPUT/metadata")"
+
+  credhub login \
+    --client-name="$(jq -r .bosh.credhub_client < "$TOOLSMITH_ENV_INPUT/metadata")" \
+    --client-secret="$(jq -r .bosh.credhub_secret < "$TOOLSMITH_ENV_INPUT/metadata")" \
+    --server="$(jq -r .bosh.credhub_server < "$TOOLSMITH_ENV_INPUT/metadata")" \
+    --ca-cert="$(jq -r .bosh.credhub_ca_cert < "$TOOLSMITH_ENV_INPUT/metadata")"
+
+  API_HOST="$(jq -r .cf.api_url < "$TOOLSMITH_ENV_INPUT/metadata")"
+  PASSWORD="$(credhub get -n "/bosh-${env_name}/cf/cf_admin_password" -q)"
+  CLIENT_SECRET="$(credhub get -n "/bosh-${env_name}/cf/uaa_admin_client_secret" -q)"
+}
 
 run_tests() {
   export SPRING_CLOUD_APPBROKER_ACCEPTANCETEST_CLOUDFOUNDRY_API_HOST="${API_HOST}"
@@ -30,6 +49,8 @@ run_tests() {
 }
 
 main() {
+  discover_environment
+
   echo "Running tests against $API_HOST"
   echo
 

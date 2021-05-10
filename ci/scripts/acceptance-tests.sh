9be0a762553f814f1b467dfd4cafb24e426968c5
Gareth Clay <gclay@pivotal.io>
2021-05-10 15:24:08 +0100

    Scale Toolsmiths environment for ATs
    * Scale to 3 Diego cells to handle AT workload
    * Rich Gradle output for ATs in CI
    * Clean more things in clean-env.sh

--- ci/scripts/acceptance-tests.sh
+++ ci/scripts/acceptance-tests.sh
@@ -7,6 +7,7 @@ readonly DEFAULT_ORG="${DEFAULT_ORG:?must be set}"
 readonly DEFAULT_SPACE="${DEFAULT_SPACE:?must be set}"
 readonly SKIP_SSL_VALIDATION="${SKIP_SSL_VALIDATION:?must be set}"
 readonly ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE:-true}"
+readonly DEPLOYMENT_DIRECTORY="$(mktemp -d)"
 
 declare API_HOST
 readonly API_PORT=443
@@ -21,12 +22,28 @@ discover_environment() {
 
   eval "$(bbl print-env --metadata-file "$TOOLSMITH_ENV_INPUT/metadata")"
 
-  API_HOST="$(jq -r .cf.api_url < "$TOOLSMITH_ENV_INPUT/metadata")"
+  API_HOST="$(jq -r .cf.api_url <"$TOOLSMITH_ENV_INPUT/metadata")"
   PASSWORD="$(credhub get -n "/bosh-${env_name}/cf/cf_admin_password" -q)"
   CLIENT_SECRET="$(credhub get -n "/bosh-${env_name}/cf/uaa_admin_client_secret" -q)"
 }
 
-prepare_environment() {
+prepare_cf_deployment() {
+  pushd "$DEPLOYMENT_DIRECTORY" > /dev/null
+
+  bosh --deployment cf manifest > manifest.yml
+
+  cat << EOF > ops.yml
+- type: replace
+  path: /instance_groups/name=diego-cell/instances
+  value: 3
+EOF
+
+  bosh --non-interactive --deployment cf deploy --ops-file ops.yml manifest.yml
+
+  popd > /dev/null
+}
+
+prepare_cf() {
   local test_instances_org
   test_instances_org="$DEFAULT_ORG-instances"
 
@@ -50,9 +67,9 @@ run_tests() {
   export SPRING_CLOUD_APPBROKER_ACCEPTANCETEST_CLOUDFOUNDRY_DEFAULT_SPACE="${DEFAULT_SPACE}"
   export SPRING_CLOUD_APPBROKER_ACCEPTANCETEST_CLOUDFOUNDRY_SKIP_SSL_VALIDATION="${SKIP_SSL_VALIDATION}"
   export TESTS_BROKERAPPPATH=build/libs/spring-cloud-app-broker-acceptance-tests.jar
-  ./gradlew -PacceptanceTests \
-  	-PonlyShowStandardStreamsOnTestFailure="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE}" \
-  	:spring-cloud-app-broker-acceptance-tests:test
+  ./gradlew --console rich -PacceptanceTests \
+    -PonlyShowStandardStreamsOnTestFailure="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE}" \
+    :spring-cloud-app-broker-acceptance-tests:test
 }
 
 main() {
@@ -61,11 +78,12 @@ main() {
   echo "Running tests against $API_HOST"
   echo
 
-  prepare_environment
+  prepare_cf_deployment
+  prepare_cf
 
-  pushd "git-repo" > /dev/null
-    run_tests
-  popd > /dev/null
+  pushd "git-repo" >/dev/null
+  run_tests
+  popd >/dev/null
 }
 
 main

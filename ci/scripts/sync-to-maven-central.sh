8834ea2435c989eeb425c9c447768f230cdaeaa2
Abel Salgado Romero <romeroab@vmware.com>
2021-07-21 12:01:03 +0200

    Remove Bintray from release pipelines

--- ci/scripts/sync-to-maven-central.sh
+++ ci/scripts/sync-to-maven-central.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
-buildName=$(cat artifactory-repo/build-info.json | jq -r '.buildInfo.name')
-buildNumber=$(cat artifactory-repo/build-info.json | jq -r '.buildInfo.number')
-version=$(cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/')
+readonly BUILD_INFO_LOCATION="$(pwd)/artifactory-repo/build-info.json"
+readonly CONFIG_DIR="$(pwd)/git-repo/ci/config"
 
-echo "Syncing ${buildName}/${buildNumber} to Maven Central"
-curl \
-  -s \
-  --connect-timeout 240 \
-  --max-time 2700 \
-  -u "${BINTRAY_USERNAME}":"${BINTRAY_API_KEY}" \
-  -H "Content-Type: application/json" -d "{\"username\": \"${SONATYPE_USER_TOKEN}\", \"password\": \"${SONATYPE_PASSWORD_TOKEN}\"}" \
-  -f \
-  -X \
-  POST "https://api.bintray.com/maven_central_sync/${BINTRAY_SUBJECT}/${BINTRAY_REPO}/${BINTRAY_PACKAGE}/versions/${version}" >/dev/null || {
-  echo "Failed to sync" >&2
-  exit 1
-}
+java -jar /opt/concourse-release-scripts*.jar \
+  --spring.config.location="${CONFIG_DIR}/release-scripts.yml" \
+  publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" "artifactory-repo"
+
 echo "Sync complete"
--- scripts/set-pipelines.sh
+++ scripts/set-pipelines.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+readonly FLY_TARGET="app-broker"
+readonly GITHUB_REPO="https://github.com/spring-cloud/spring-cloud-app-broker"
+
 set_pipeline() {
 	local pipeline_name pipeline_definition branch ci_image_tag
 	pipeline_name="${1:?pipeline name must be provided}"
@@ -10,10 +13,11 @@ set_pipeline() {
 	ci_image_tag="${4:-$branch}"
 
 	echo "Setting $pipeline_name pipeline..."
-	fly --target app-broker set-pipeline \
+	fly --target "$FLY_TARGET" set-pipeline \
 		--pipeline "$pipeline_name" \
 		--config "$pipeline_definition" \
 		--load-vars-from config-concourse.yml \
+		--var "github-repo=$GITHUB_REPO" \
 		--var "branch=$branch" \
 		--var "ci-image-tag=$ci_image_tag"
 }

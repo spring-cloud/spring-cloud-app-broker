120ace03c15c41b5099ecc92886433e2728368d0
Alberto C. Ríos <albertoimpl@gmail.com>
2022-01-07 12:11:49 +0100

    Upating build dist repository

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -4,10 +4,8 @@ set -euo pipefail
 
 readonly ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE:-true}"
 readonly SKIP_TESTS="${SKIP_TESTS:-false}"
+readonly DISTRIBUTION_REPOSITORY_OUTPUT="${DISTRIBUTION_REPOSITORY_OUTPUT:?must be set}"
 
-# shellcheck source=common.sh
-source "$(dirname "$0")/common.sh"
-repository=$(pwd)/distribution-repository
 if [ "$SKIP_TESTS" == "true" ]; then
 	build_task=assemble
 else
@@ -16,4 +14,4 @@ fi
 
 ./gradlew --parallel clean "$build_task" publish \
 	-PonlyShowStandardStreamsOnTestFailure="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE}" \
-	-PpublicationRepository="${repository}"
+	-PpublicationRepository="${DISTRIBUTION_REPOSITORY_OUTPUT}"

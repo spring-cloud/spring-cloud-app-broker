db0d90ce69abb5cbdb961dcadfaf557a73a29914
Alberto C. Ríos <albertoimpl@gmail.com>
2022-01-10 13:17:40 +0100

    Reverting distribution repository
    To re-enable snapshot publishing

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -4,14 +4,18 @@ set -euo pipefail
 
 readonly ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE:-true}"
 readonly SKIP_TESTS="${SKIP_TESTS:-false}"
-readonly DISTRIBUTION_REPOSITORY_OUTPUT="${DISTRIBUTION_REPOSITORY_OUTPUT:?must be set}"
 
+# shellcheck source=common.sh
+source "$(dirname "$0")/common.sh"
+repository=$(pwd)/distribution-repository
 if [ "$SKIP_TESTS" == "true" ]; then
 	build_task=assemble
 else
 	build_task=build
 fi
 
+pushd git-repo >/dev/null
 ./gradlew --parallel clean "$build_task" publish \
 	-PonlyShowStandardStreamsOnTestFailure="${ONLY_SHOW_STANDARD_STREAMS_ON_TEST_FAILURE}" \
-	-PpublicationRepository="${DISTRIBUTION_REPOSITORY_OUTPUT}"
+	-PpublicationRepository="${repository}"
+popd >/dev/null

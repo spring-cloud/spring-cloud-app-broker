4aadd691140624a69b83389391cca034b5807d07
Alberto C. Ríos <albertoimpl@gmail.com>
2022-05-20 09:39:55 +0200

    Updating boot to 2.7.0

--- spring-cloud-app-broker-logging/src/test/java/org/springframework/cloud/appbroker/logging/streaming/ServiceInstanceLogStreamingTest.java
+++ spring-cloud-app-broker-logging/src/test/java/org/springframework/cloud/appbroker/logging/streaming/ServiceInstanceLogStreamingTest.java
@@ -42,7 +42,7 @@ import reactor.core.publisher.Mono;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.cloud.appbroker.logging.LoggingUtils;
 import org.springframework.cloud.appbroker.logging.streaming.events.ServiceInstanceLogEvent;
 import org.springframework.context.ApplicationEventPublisher;

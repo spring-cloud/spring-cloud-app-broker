e92c9996ab09d9c82fcaa286fbbfa70445e5911d
Roy Clarkson <roy.clarkson@broadcom.com>
2025-02-04 16:59:31 -0500

    Add pipeline for 2.4.x branch

--- scripts/set-pipelines.sh
+++ scripts/set-pipelines.sh
@@ -10,7 +10,7 @@ readonly PIPELINE_TYPE=${1:-""}
 
 set_branch_pipeline() {
   local -r pipeline_name="app-broker${PIPELINE_NAME_SUFFIX:+"-$PIPELINE_NAME_SUFFIX"}"
-  local -r branches=("2.3.x" "2.2.x" "2.1.x" "2.0.x" "1.6.x")
+  local -r branches=("2.4.x" "2.3.x" "2.2.x" "2.1.x" "2.0.x" "1.6.x")
 
 	for branch in "${branches[@]}"; do
   	echo "Setting $pipeline_name $branch pipeline..."
